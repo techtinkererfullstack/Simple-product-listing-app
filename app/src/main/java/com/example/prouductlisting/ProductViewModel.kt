c4df240ba2083dbf4db47de1d973275945c85819
shafe <alam.shafee@gmail.com>
2026-02-25 21:22:38 +0600

    added viewModel

--- app/src/main/java/com/example/prouductlisting/ProductViewModel.kt
+++ app/src/main/java/com/example/prouductlisting/ProductViewModel.kt
@@ -1,4 +1,41 @@
 package com.example.prouductlisting
 
-class ProductViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+class ProductViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: ProductRipository
+    val productLiveData = MutableLiveData<List<Product>>()
+
+    init {
+        val dao = AppDatabase.getDatabase(application).productDao()
+        repository = ProductRipository(dao)
+        getAllProductsViewModel()
+    }
+
+    fun getAllProductsViewModel() {
+        productLiveData.value = repository.getAllProductsRepo()
+    }
+
+    fun insertProductViewModel(product: Product) {
+        repository.insertProductRepo(product)
+        getAllProductsViewModel()
+    }
+
+    fun updateProductViewModel(product: Product) {
+        repository.updateProductRepo(product)
+        getAllProductsViewModel()
+    }
+
+    fun deleteProductViewModel(product: Product) {
+        repository.deleteProductRepo(product)
+        getAllProductsViewModel()
+    }
+
+
 }
+
+
+
+
